e226d3ef38bf740a31b47519b0b4f84f6a0959e9
con-mahlbacher <97570210+con-mahlbacher@users.noreply.github.com>
2023-11-02 14:37:29 -0400

    revert back to last working version

--- server.py
+++ server.py
@@ -1,36 +1,20 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
-from pyramid.view import view_config
 import os
 
-students = [
-   {"id": 1, "name": "Ravi", "percent": 75},
-   {"id": 2, "name": "Mona", "percent": 80},
-   {"id": 3, "name": "Mathews", "percent": 45},
-]
-
-@view_config(route_name='index', renderer='templates/myform.html')
-def index(request):
-    return {}
-
-@view_config(route_name='students', renderer='templates/hello.html')
-def add(request):
-    student = {'id':request.params['id'],
-               'name':request.params['name'],
-               'percent':int(request.params['percent'])}
-    students.append(student)
-    return {'students':students}
-
+def hello_world(request):
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Good morning, " + name + "!\n"
+    return Response(message)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
     with Configurator() as config:
-        config.include('pyramid_jinja2')
-        config.add_jinja2_renderer(".html")
-        config.add_route('index', '/')
-        config.add_route('students', '/students')
-        config.scan()
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
     server.serve_forever()
