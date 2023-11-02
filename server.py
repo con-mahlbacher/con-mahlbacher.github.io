05de218ff232e769bee7fd110c22ef5b6e516797
con-mahlbacher <97570210+con-mahlbacher@users.noreply.github.com>
2023-11-02 16:36:47 -0400

    step 3

--- server.py
+++ server.py
@@ -5,17 +5,18 @@ from pyramid.renderers import render
 from pyramid.view import view_config
 import os
 
-@view_config(route_name='hello', renderer='templates/bicdex.html')
+#@view_config(route_name='hello')
 def hello_world(request):
-    return {}
+    return Response("Hello :(")
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
     with Configurator() as config:
-        config.include('pyramid_jinja2')
-        config.add_jinja2_renderer(".html")
+        #config.include('pyramid_jinja2')
+        #config.add_jinja2_renderer(".html")
         config.add_route('hello', '/')
-        config.scan()
+        config.add_view(hello_world, route_name='hello')
+        #config.scan()
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
     server.serve_forever()
