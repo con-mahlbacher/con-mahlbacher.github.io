708db1285ac1b8d0d29156e2922576e3236022f5
con-mahlbacher <97570210+con-mahlbacher@users.noreply.github.com>
2023-11-02 13:50:46 -0400

    Try again

--- server.py
+++ server.py
@@ -4,9 +4,24 @@ from pyramid.response import Response
 from pyramid.view import view_config
 import os
 
-@view_config(route_name='index', renderer='index.html')
+students = [
+   {"id": 1, "name": "Ravi", "percent": 75},
+   {"id": 2, "name": "Mona", "percent": 80},
+   {"id": 3, "name": "Mathews", "percent": 45},
+]
+
+@view_config(route_name='index', renderer='templates/myform.html')
 def index(request):
     return {}
+
+@view_config(route_name='students', renderer='templates/hello.html')
+def add(request):
+    student = {'id':request.params['id'],
+               'name':request.params['name'],
+               'percent':int(request.params['percent'])}
+    students.append(student)
+    return {'students':students}
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
@@ -14,6 +29,7 @@ if __name__ == '__main__':
         config.include('pyramid_jinja2')
         config.add_jinja2_renderer(".html")
         config.add_route('index', '/')
+        config.add_route('students', '/students')
         config.scan()
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
