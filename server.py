323b72fe03e1839335730339d8389ec291b549ed
con-mahlbacher <97570210+con-mahlbacher@users.noreply.github.com>
2023-11-02 13:21:56 -0400

    big changes coming up the pipe

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+from pyramid.view import view_config
 import os
 
-def hello_world(request):
-    name = os.environ.get('NAME')
-    if name == None or len(name) == 0:
-        name = "world"
-    message = "Good morning, " + name + "!\n"
-    return Response(message)
+@view_config(route_name='index', renderer='index.html')
+def index(request):
+    return {}
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
     with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
+        config.include('pyramid_jinja2')
+        config.add_jinja2_renderer(".html")
+        config.add_route('index', '/')
+        config.scan()
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
     server.serve_forever()
