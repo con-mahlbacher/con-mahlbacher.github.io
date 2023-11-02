1f9b1a24a2f925cc91c8e3f5584a9ceac4fcf7ce
con-mahlbacher <97570210+con-mahlbacher@users.noreply.github.com>
2023-11-02 15:20:30 -0400

    gimme dat bicdex

--- server.py
+++ server.py
@@ -1,20 +1,21 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+from pyramid.renderers import render
+from pyramid.view import view_config
 import os
 
+@view_config(route_name='hello', renderer='templates/bicdex.html')
 def hello_world(request):
-    name = os.environ.get('NAME')
-    if name == None or len(name) == 0:
-        name = "world"
-    message = "Good morning, " + name + "!\n"
-    return Response(message)
+    return {}
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
     with Configurator() as config:
+        config.include('pyramid_jinja2')
+        config.add_jinja2_renderer(".html")
         config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
+        config.scan()
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
     server.serve_forever()
