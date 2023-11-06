6cf09eb8be4b291f80952d7a7ec67c137768f44d
con-mahlbacher <97570210+con-mahlbacher@users.noreply.github.com>
2023-11-06 12:11:52 -0500

    pyramid dead

--- server.py
+++ server.py
@@ -4,16 +4,17 @@ from pyramid.response import Response
 from pyramid.renderers import render
 from pyramid.view import view_config
 from pyramid.renderers import render_to_response
+from pyramid import pyramid_jinja2
 import os
 
 #@view_config(route_name='hello')
 def hello_world(request):
-    return render_to_response('templates/hello.jinja2', {'name':'Tutorialspoint'}, request=request)
+    return "render_to_response('templates/hello.jinja2', {'name':'Tutorialspoint'}, request=request)"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
     with Configurator() as config:
-        config.include('pyramid_jinja2')
+        #config.include('pyramid_jinja2')
         #config.add_jinja2_renderer(".html")
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
